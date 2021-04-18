d5cc9ce8110bc058122e314ce0059832a032c0da
Peter Ihimire <peterihimire@gmail.com>
2021-04-18 19:29:11 +0100

    Still working on the usersupdatepage component

--- src/user/pages/UsersUpdatePage.js
+++ src/user/pages/UsersUpdatePage.js
@@ -11,7 +11,6 @@ function ValidationMessage(props) {
 class UpdatePropertiesPage extends React.Component {
   static contextType = AuthContext;
   context = this.context;
-  
 
   constructor(props) {
     super(props);
@@ -21,7 +20,7 @@ class UpdatePropertiesPage extends React.Component {
     console.log(propertyId);
 
     this.state = {
-      property: {},
+      user: {},
       name: "",
       nameValid: true,
       slug: "",
@@ -90,7 +89,7 @@ class UpdatePropertiesPage extends React.Component {
   componentDidMount() {
     // GETTING THE PROPERTY ID VIA PAGE-URL-PARAMS
     // let propertyId = this.props.match.params.propertyId;
-    console.log(this.context)
+    console.log(this.context);
     const fetchProperty = () => {
       fetch(`http://localhost:4000/api/admin/users/${this.context.userId} `)
         .then((response) => {
@@ -105,17 +104,12 @@ class UpdatePropertiesPage extends React.Component {
               // this.setState({ loading: false });
               console.log(response);
               this.setState({
-                // property: res.property,
-                // name: res.property.name || "",
-                // slug: res.property.slug || "",
-                // location: res.property.location || "",
-                // amount: res.property.amount || "",
-                // completion: res.property.completion || "",
-                // description: res.property.description || "",
-                // image: res.property.image || "",
-                // preview: `http://localhost:4000/${res.property.image}`,
+                user: res.user,
+                name: res.user.name || "",
+                image: res.user.image || "",
+                preview: `http://localhost:4000/${res.user.image}`,
               });
-              let property = res.property;
+              let property = res.user;
               console.log(property);
               // console.log(property.image);
               // return property;
@@ -185,96 +179,6 @@ class UpdatePropertiesPage extends React.Component {
     this.setState({ nameValid, errorMsg }, this.validateForm);
   };
 
-  // VALIDITY FOR SLUG
-  updateSlug = (slug) => {
-    this.setState({ slug }, this.validateSlug);
-  };
-
-  validateSlug = () => {
-    const { slug } = this.state.property;
-    let slugValid = true;
-    let errorMsg = { ...this.state.errorMsg };
-
-    if (slug.length < 3) {
-      slugValid = false;
-      errorMsg.slug = "Must be at least 3 characters long";
-    }
-
-    this.setState({ slugValid, errorMsg }, this.validateForm);
-  };
-
-  // VALIDITY FOR LOCATION
-  updateLocation = (location) => {
-    this.setState({ location }, this.validateLocation);
-  };
-
-  validateLocation = () => {
-    const { location } = this.state.property;
-    let locationValid = true;
-    let errorMsg = { ...this.state.errorMsg };
-
-    if (location.length < 3) {
-      locationValid = false;
-      errorMsg.location = "Must be at least 3 characters long";
-    }
-
-    this.setState({ locationValid, errorMsg }, this.validateForm);
-  };
-
-  // VALIDITY FOR AMOUNT
-  updateAmount = (amount) => {
-    this.setState({ amount }, this.validateAmount);
-  };
-
-  validateAmount = () => {
-    const { amount } = this.state.property;
-    let amountValid = true;
-    let errorMsg = { ...this.state.errorMsg };
-
-    if (amount.length < 3) {
-      amountValid = false;
-      errorMsg.amount = "Must be at least 3 characters long";
-    }
-
-    this.setState({ amountValid, errorMsg }, this.validateForm);
-  };
-
-  // VALIDITY FOR COMPLETION
-  updateCompletion = (completion) => {
-    this.setState({ completion }, this.validateCompletion);
-  };
-
-  validateCompletion = () => {
-    const { completion } = this.state;
-    let completionValid = true;
-    let errorMsg = { ...this.state.errorMsg };
-
-    if (completion.length < 3) {
-      completionValid = false;
-      errorMsg.completion = "Must be at least 3 characters long";
-    }
-
-    this.setState({ completionValid, errorMsg }, this.validateForm);
-  };
-
-  // VALIDITY FOR DESCRIPTION
-  updateDescription = (description) => {
-    this.setState({ description }, this.validateDescription);
-  };
-
-  validateDescription = () => {
-    const { description } = this.state.property;
-    let descriptionValid = true;
-    let errorMsg = { ...this.state.errorMsg };
-
-    if (description.length < 3) {
-      descriptionValid = false;
-      errorMsg.description = "Must be at least 3 characters long";
-    }
-
-    this.setState({ descriptionValid, errorMsg }, this.validateForm);
-  };
-
   // VALIDITY FOR IMAGE
   updateImage = (e) => {
     console.log(e.target.files[0]);
@@ -308,24 +212,15 @@ class UpdatePropertiesPage extends React.Component {
 
     const data = {
       name: this.state.name,
-      slug: this.state.slug,
-      location: this.state.location,
-      amount: this.state.amount,
-      completion: this.state.completion,
-      description: this.state.description,
-      image: this.state.image,
+  
     }; // Sending this to the backend
 
     const formData = new FormData();
     formData.append("name", this.state.name);
-    formData.append("slug", this.state.slug);
-    formData.append("location", this.state.location);
-    formData.append("amount", this.state.amount);
-    formData.append("completion", this.state.completion);
-    formData.append("description", this.state.description);
+ 
     // formData.append("image", this.state.image);
 
-    fetch(`http://localhost:4000/api/admin/properties/${propertyId}`, {
+    fetch(`http://localhost:4000/api/admin/users/${propertyId}`, {
       method: "PUT",
       // headers: {
       //   "Content-Type": "application/json",
@@ -371,7 +266,7 @@ class UpdatePropertiesPage extends React.Component {
     return (
       <div>
         <div className="App">
-          <h4>Updated Property Form</h4>
+          <h4>Updated User Form</h4>
 
           <form action="#" id="js-form" onSubmit={this.propertySubmitHandler}>
             <div className="form-group">
@@ -388,87 +283,6 @@ class UpdatePropertiesPage extends React.Component {
                 value={this.state.name || ""}
                 onChange={(e) => this.updateName(e.target.value)}
               />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="slug">slug</label>
-              <ValidationMessage
-                valid={this.state.slugValid}
-                message={this.state.errorMsg.slug}
-              />
-              <input
-                type="text"
-                id="slug"
-                name="slug"
-                className="form-field"
-                value={this.state.slug || ""}
-                onChange={(e) => this.updateSlug(e.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="location">location</label>
-              <ValidationMessage
-                valid={this.state.locationValid}
-                message={this.state.errorMsg.location}
-              />
-              <input
-                type="text"
-                id="location"
-                name="location"
-                className="form-field"
-                value={this.state.location || ""}
-                onChange={(e) => this.updateLocation(e.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="amount">amount</label>
-              <ValidationMessage
-                valid={this.state.amountValid}
-                message={this.state.errorMsg.amount}
-              />
-              <input
-                type="text"
-                id="amount"
-                name="amount"
-                className="form-field"
-                value={this.state.amount || ""}
-                onChange={(e) => this.updateAmount(e.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="completion">completion</label>
-              <ValidationMessage
-                valid={this.state.completionValid}
-                message={this.state.errorMsg.completion}
-              />
-              <input
-                type="text"
-                id="completion"
-                name="completion"
-                className="form-field"
-                value={this.state.completion || ""}
-                onChange={(e) => this.updateCompletion(e.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="description">description</label>
-              <ValidationMessage
-                valid={this.state.descriptionValid}
-                message={this.state.errorMsg.description}
-              />
-              <textarea
-                type="text"
-                id="description"
-                name="description"
-                rows="8"
-                className="form-field-2"
-                value={this.state.description || ""}
-                onChange={(e) => this.updateDescription(e.target.value)}
-              ></textarea>
             </div>
 
             <div className="form-group">
@@ -514,55 +328,13 @@ class UpdatePropertiesPage extends React.Component {
               />
             </div>
 
-            {/* <div className="form-group">
-              <label htmlFor="image">image</label>
-
-              <ValidationMessage
-                valid={this.state.completionValid}
-                message={this.state.errorMsg.completion}
-              />
-              {this.state.preview ? (
-                <Link to="/update-image" className="">
-                  <div className="preview-img">
-                    <img
-                      src={this.state.preview}
-                      onClick={() => {
-                        this.setState({ image: "", preview: null });
-                      }}
-                    />
-                  </div>
-                </Link>
-              ) : (
-                <button
-                  className="form-image-btn"
-                  // the below function with click method launches the file input
-                  onClick={(e) => {
-                    e.preventDefault();
-                    this.fileInputRef.current.click();
-                  }}
-                >
-                  add image
-                </button>
-              )}
-
-              <input
-                type="file"
-                id="image"
-                name="image"
-                className="form-file"
-                accept=".jpg, .png, .jpeg"
-                onChange={this.updateImage}
-                ref={this.fileInputRef}
-              />
-            </div> */}
-
             <div className="form-controls">
               <button
                 className="button"
                 type="submit"
                 disabled={!this.state.formValid}
               >
-                Add Property
+                Update
               </button>
             </div>
           </form>
